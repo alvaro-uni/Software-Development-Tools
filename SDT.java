8a7f8d75f1c85d9c20e0f7c50588b93ceb610a31
alvaro-uni <alvaro.nicolasy@um.es>
2025-12-03 17:10:22 +0100

    n

--- SDT.java
+++ SDT.java
@@ -6,12 +6,12 @@ public class SDT {
 
         int[] table = new int[10];
         Random rand = new Random();
-
+        int n = 0;
         for (int i = 0; i < table.length; i++) {
             table[i] = rand.nextInt(100);
         }
 
-        System.out.println("Disordered array:");
+        System.out.println("Disordered array :");
         System.out.println(Arrays.toString(table));
 
         Arrays.sort(table);
