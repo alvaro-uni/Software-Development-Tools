eee07237701cbc9a4a4691f1f7cbd6f43dc6a223
alvaro-uni <alvaro.nicolasy@um.es>
2025-12-03 16:53:57 +0100

    array

---
+++ SDT.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] table = {3, 8, 2, 5};
+
+        System.out.println("Array elements:");
+        for (int value : table) {
+            System.out.println(value);
+        }
+    }
+}
