59c36d359df4a7a442a856bfe51bf983d1c947bf
alvaro-uni <alvaro.nicolasy@um.es>
2025-12-03 16:55:30 +0100

    random array

--- SDT.java
+++ SDT.java
@@ -1,8 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int[] table = {3, 8, 2, 5};
 
-        System.out.println("Array elements:");
+        int[] table = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < table.length; i++) {
+            table[i] = rand.nextInt(100); 
+        }
+
+        System.out.println("Random values array:");
         for (int value : table) {
             System.out.println(value);
         }
